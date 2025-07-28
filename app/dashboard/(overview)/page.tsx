dc2029157674c9a5b9a97a944d722d6a4d949e66
kwokd <43962429+kwokd@users.noreply.github.com>
2025-07-28 21:35:07 +0100

    -c16

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -7,6 +7,12 @@ import { fetchCardData } from '@/app/lib/data';
 import { Suspense } from 'react';
 import { RevenueChartSkeleton, LatestInvoicesSkeleton, CardsSkeleton } from '@/app/ui/skeletons';
 
+import { Metadata } from 'next';
+ 
+export const metadata: Metadata = {
+  title: 'Dashboard',
+};
+
 export default async function Page() {
     const {
       numberOfCustomers,
